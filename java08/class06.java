ad57884e0a96cf48466866e45d048d507642c131
Meloncowboy <86897937+Meloncowboy@users.noreply.github.com>
2022-09-21 10:27:53 +0800

    Add files via upload

--- java08/class06.java
+++ java08/class06.java
@@ -7,11 +7,18 @@ public class class06 {
 		void show_periphery() {
 			System.out.println("periphery="+2*pi*radius);
 		}
+		void setpi(double a) {
+			this.pi = a;
+		}
 	}
 	public static void main(String[] args) {
 		CCircle cir1 = new CCircle();
 		cir1.radius = 3.0;
 		cir1.show_periphery();
+		
+		System.out.println("pi="+cir1.pi);
+		cir1.setpi(3.15);
+		System.out.println("pi="+cir1.pi);
 	}
 
 }
