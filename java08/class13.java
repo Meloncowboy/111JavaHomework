51da0b6c864719a348736b3d30f421a0638ea8dc
Meloncowboy <86897937+Meloncowboy@users.noreply.github.com>
2022-09-21 11:59:19 +0800

    Add files via upload

---
+++ java08/class13.java
@@ -0,0 +1,23 @@
+package abc;
+
+class CCircle{
+	double pi = 3.14;
+	float radius = 10;
+	
+	double area(double r) {
+		
+		return radius * radius * pi;
+	}
+	float area(float r) {
+		
+		return radius * radius * pi;
+	}
+}
+public class class13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
