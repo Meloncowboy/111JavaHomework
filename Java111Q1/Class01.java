294bc8a5c078a53e32400b18aecf24f88fb99d4c
Meloncowboy <86897937+Meloncowboy@users.noreply.github.com>
2022-10-05 11:49:23 +0800

    Add files via upload

--- Java111Q1/Class01.java
+++ Java111Q1/Class01.java
@@ -6,21 +6,23 @@ class a1{
 	double pi = 3.14159;
 	
 	public a1(int r) {
-		Cal_area(r);
+		this.r = r;
 	}
 	public a1(int x, int y, int z) {
-		Cal_area(x,y,z);
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 	
-	void Cal_area(int r) {
-		System.out.println("半徑 = " + r);
-		System.out.printf("球形體積 = %.1f\n", (double)(4 / 3) * pi * r * r * r);
-	}
-	
-	void Cal_area(int x, int y, int z) {
-		int outsize = x * y * 2 + x * z * 2 + y * z *2;
-		System.out.printf("長 = %d ,寬 = %d ,高 = %d\n", x, y, z);	
-		System.out.println("立方體表面積 = " + outsize);	
+	void Cal_area() {
+		if(r != 0) {
+			System.out.println("半徑=" + r);
+			System.out.printf("球形體積=%.1f\n", (double)(4 / 3) * pi * r * r * r);	
+		}else {
+			int outsize = x * y * 2 + x * z * 2 + y * z *2;
+			System.out.printf("長=%d,寬=%d,高=%d\n", x, y, z);	
+			System.out.println("立方體表面積=" + outsize);	
+		}
 	}
 }
 public class Class01 {
@@ -28,6 +30,8 @@ public class Class01 {
 	public static void main(String[] args) {
 		a1 a = new a1(10);
 		a1 b = new a1(4, 5, 6);
+		a.Cal_area();
+		b.Cal_area();
 	}
 
 }
