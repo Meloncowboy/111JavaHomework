68a0b7d5c4276d4885fdcf942825b11626925bf9
Meloncowboy <86897937+Meloncowboy@users.noreply.github.com>
2022-11-16 12:07:36 +0800

    Add files via upload

---
+++ Java12/Class08/Class08.java
@@ -0,0 +1,9 @@
+import subsubpack.CStyle;
+public class Class08 {
+
+	public static void main(String[] args) {
+		CStyle a = new CStyle(1);
+
+	}
+
+}
---
+++ Java12/Class08/subsubpack/CStyle.java
@@ -0,0 +1,11 @@
+package subsubpack;
+
+public class CStyle {
+
+	public int style;
+	
+	public CStyle(int n) {
+		style = n;
+		System.out.println("style"+style);
+	}
+}
